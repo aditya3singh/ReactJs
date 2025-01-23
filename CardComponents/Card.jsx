fd22b98895e8a92e9da03251a55b4195ae9d2c3d
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-01-23 19:39:37 +0530

    Update Card.jsx

--- CardComponents/Card.jsx
+++ CardComponents/Card.jsx
@@ -1,1 +1,13 @@
+import logo from './assets/LOGO.png';
 
+function Card(){
+    return(
+    <div className="card">
+        <img className="card-img" src={logo} alt="logo"/>
+        <h2 className='card-title'>Hi Hello! </h2>
+        <p className='card-text'>I am student and learning React library</p>
+    </div>
+    );
+}
+
+export default Card;
