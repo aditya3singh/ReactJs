f718094cee337054b3d730ee4463fdbe394e03c9
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-01-23 19:02:02 +0530

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <header>
+            <h1>My React website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">project</a></li>
+                    <li><a href="#">blog</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+
+        </header>
+    );
+}
+
+
+export default Header;
