37d8f26377ec204f6b480b1a970569aac457f22b
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-01-25 00:09:10 +0530

    Create PropTypesJust.jsx

---
+++ PropTypesJust.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PropTypesJust from './PropTypesJust';
+
+function App() {
+
+  return (
+    <>
+      <PropTypesJust name="Aditya" age={21}  />
+    </>
+  );
+}
+
+export default App
